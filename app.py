1f738eacdc843be64ede328a0b2242f1b749f26c
SelvarajManikkalai <selvaraj.manikkalai1995@gmail.com>
2024-03-12 22:22:35 +0530

    aded

--- app.py
+++ app.py
@@ -225,4 +225,10 @@ def search():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    # app.run(debug=True)
+    app.run(debug=True)
+
+#
+# if __name__ == "__main__":
+#     with app.app_context():
+#         db.create_all()
+#         app.run(debug=True)
